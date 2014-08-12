b44e1f99e3ca57a46973bc9c96bdf288a950a063
Marcos Vanetta <marcosvanetta@gmail.com>
2014-08-12 12:46:04 -0500

    Add helper

--- lib/helpers.py
+++ lib/helpers.py
@@ -2,9 +2,12 @@ from boot import session
 
 
 def store_objects(objects):
-    for object in objects:
-        try:
-          session.add(object)
-          session.commit()
-        except Exception as e:
-          session.rollback()
+    for obj in objects:
+        store_object(obj)
+
+def store_object(obj):
+    try:
+        session.add(obj)
+        session.commit()
+    except Exception as e:
+        session.rollback()
