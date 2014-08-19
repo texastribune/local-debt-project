61e15eaac1b3f0bd957608da753258e882d27bd7
Marcos Vanetta <marcosvanetta@gmail.com>
2014-08-19 15:20:33 -0500

    Add search and location api endpoints

---
+++ debt_compare/location_service.py
@@ -0,0 +1,14 @@
+from debt_compare.models import CityDebt, CountyDebt, SchoolDistrictDebt
+
+
+class LocationService(object):
+    def __init__(self, lat, lng):
+        self.lat = lat
+        self.lng = lng
+
+    def __call__(self):
+        return [
+            CityDebt.objects.get(id=1),
+            CountyDebt.objects.get(id=1),
+            SchoolDistrictDebt.objects.get(id=1)
+        ]
