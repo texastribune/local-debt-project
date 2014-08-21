9f68f5e260792043a2d2762046f14f4ab51b61ab
Marcos Vanetta <marcosvanetta@gmail.com>
2014-08-21 14:48:25 -0500

    Render entities with new layout

--- webapp/app/scripts/controllers/main_controller.js
+++ webapp/app/scripts/controllers/main_controller.js
@@ -17,15 +17,45 @@ app.Controllers.Main = Marionette.Controller.extend({
     this.entities.push(city);
     this.entities.push(ids);
 
+    this.renderButtons();
+    this.renderCounty(city);
+    this.renderCity(city);
+    this.renderIDS(ids);
+  },
+
+  renderCounty: function(county) {
+    'use strict';
+    this.countyLayout = new app.Layouts.EntityLayout();
+    app.countyRegion.show(this.countyLayout);
+    this.countyLayout.debtBox.show(new app.Views.Entity({
+      model: county
+    }));
+  },
+
+  renderCity: function(city) {
+    'use strict';
+    this.cityLayout = new app.Layouts.EntityLayout();
+    app.cityRegion.show(this.cityLayout);
+    this.cityLayout.debtBox.show(new app.Views.Entity({
+      model: city
+    }));
+  },
+
+  renderIDS: function(ids) {
+    'use strict';
+    this.isdRegion = new app.Layouts.EntityLayout();
+    app.isdRegion.show(this.isdRegion);
+    this.isdRegion.debtBox.show(new app.Views.Entity({
+      model: ids
+    }));
+  },
+
+  renderButtons: function() {
+    'use strict';
     this.goToButtons = new app.Views.GoToButtons({
       collection: this.entities
     });
 
-    this.entitiesView = new app.Views.Entities({
-      collection: this.entities
-    });
-
     app.goToRegion.show(this.goToButtons);
-    app.cardsRegion.show(this.entitiesView);
   }
 });
