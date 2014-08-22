62f90841b2fae2732f9ebe64ff0e645b7bf2df62
Marcos Vanetta <marcosvanetta@gmail.com>
2014-08-22 14:59:12 -0500

    specialize population context

--- webapp/app/scripts/views/population_context_view.js
+++ webapp/app/scripts/views/population_context_view.js
@@ -1,5 +1,5 @@
 app.Views.PopulationContext = Marionette.CompositeView.extend({
   template: JST['app/scripts/templates/population_context.html'],
-  childView: app.Views.ContextRow,
+  childView: app.Views.PopulationContextRow,
   childViewContainer: 'tbody',
 });
