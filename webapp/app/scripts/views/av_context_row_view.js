2628291bebdf3f118c6fa433a17d636536de7cda
Becca Aaronson <becca.aaronson@gmail.com>
2014-08-22 17:20:53 -0500

    apply formatters, fix table styles

--- webapp/app/scripts/views/av_context_row_view.js
+++ webapp/app/scripts/views/av_context_row_view.js
@@ -2,3 +2,5 @@ app.Views.AVContextRow = Marionette.ItemView.extend({
   template: JST['app/scripts/templates/av_context_row.html'],
   tagName: 'tr'
 });
+
+_.extend(app.Views.AVContextRow.prototype, app.Mixins.Helpers);
--- webapp/app/scripts/views/population_context_row_view.js
+++ webapp/app/scripts/views/population_context_row_view.js
@@ -2,3 +2,5 @@ app.Views.PopulationContextRow = Marionette.ItemView.extend({
   template: JST['app/scripts/templates/population_context_row.html'],
   tagName: 'tr'
 });
+
+_.extend(app.Views.PopulationContextRow.prototype, app.Mixins.Helpers);
