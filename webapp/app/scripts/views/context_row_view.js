da0790e5566ca6a73211c172fb9882713e655247
Marcos Vanetta <marcosvanetta@gmail.com>
2014-08-22 14:15:45 -0500

    Show county context

---
+++ webapp/app/scripts/views/context_row_view.js
@@ -0,0 +1,4 @@
+app.Views.ContextRow = Marionette.ItemView.extend({
+  template: JST['app/scripts/templates/context_row.html'],
+  tagName: 'tr'
+});
---
+++ webapp/app/scripts/views/population_context_view.js
@@ -0,0 +1,4 @@
+app.Views.PopulationContext = Marionette.CompositeView.extend({
+  template: JST['app/scripts/templates/population_context.html'],
+  childView: app.Views.ContextRow
+});
